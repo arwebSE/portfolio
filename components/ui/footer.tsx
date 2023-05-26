f709efa1f02af9d0d11868d9d33aeaf30a755d62
August M R <contact@arweb.dev>
2023-05-26 14:46:11 +0200

    lunch

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer>
             <div className="max-w-6xl mx-auto px-4 sm:px-6">
                 {/* Top area: Blocks */}
-                <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
+                <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200 dark:border-gray-800">
                     {/* 1st block */}
                     <div className="sm:col-span-12 lg:col-span-3">
                         <div className="mb-2">
@@ -200,7 +200,7 @@ export default function Footer() {
                 </div>
 
                 {/* Bottom area */}
-                <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
+                <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200 dark:border-gray-800">
                     {/* Social as */}
                     <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
                         <li className="mr-4">
