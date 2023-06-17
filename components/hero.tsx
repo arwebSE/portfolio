8cc0de52fc9632876c034618815f2860700f33ec
August M R <contact@arweb.dev>
2023-06-17 11:21:09 +0200

    first fold ok

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,15 +1,26 @@
-import VideoThumb from "@/public/images/hero-image.png";
+import VideoThumb from "@/public/img/bg.png";
 import ModalVideo from "@/components/modal-video";
+import Image from "next/image";
 
 export default function Hero() {
     return (
         <section className="relative">
+            <div
+                className="absolute left-1/2 transform -translate-x-1/2 top-12 pointer-events-none -z-1 dark:bg-gradient-to-b opacity-50 dark:opacity-100 dark:z-0 dark:transition-opacity dark:duration-500 ease-in-out dark:delay-300 dark:filter dark:brightness-75"
+                aria-hidden="true"
+            >
+                <Image
+                    src={VideoThumb}
+                    alt="Video thumbnail"
+                    className="object-cover w-full h-full"
+                />
+            </div>
+
             <div className="flex min-h-screen flex-col items-center justify-between p-24 bg dark:bg-gradient-to-b from-white dark:to-black/70 dark:from-slate-900">
                 {/* Hero content */}
                 <div className="pt-32 pb-12 md:pt-40 md:pb-20 relative flex place-items-center before:absolute before:h-[300px] before:w-[450px] before:-translate-x-1 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:content-[''] after:absolute after:-z-20 after:h-[200px] after:w-[240px] after:translate-x-1/2 after:bg-gradient-radial after:from-sky-200 after:via-blue-200 after:content-[''] before:dark:bg-gradient-to-b before:dark:from-transparent before:dark:to-blue-700  after:dark:from-sky-900 after:dark:via-[#006AED] before:dark:opacity-30 after:dark:opacity-80 before:lg:h-[400px] before:blur-2xl after:blur-xl">
                     {/* Section header */}
-                    {/* before:blur-2xl after:blur-2xl */}
-                    <div className="text-center pb-12 md:pb-16">
+                    <div className="text-center pb-12 md:pb-16 dark:px-8 dark:py-8 dark:z-10 backdrop-blur-md dark:backgrop-blur-md rounded-2xl dark:bg-gradient-to-b dark:from-transparent dark:to-black-100">
                         <h1
                             className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
                             data-aos="zoom-y-out"
@@ -22,7 +33,7 @@ export default function Hero() {
                         </h1>
                         <div className="max-w-3xl mx-auto">
                             <p
-                                className="text-xl text-gray-600 mb-8"
+                                className="text-xl text-gray-600 mb-8 dark:text-gray-200"
                                 data-aos="zoom-y-out"
                                 data-aos-delay="150"
                             >
